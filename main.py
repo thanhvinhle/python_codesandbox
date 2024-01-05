0f3e0f45ff285acdf11c2aaffaf8ba95580c2e1b
tle <thanhvinhle@yahoo.com>
2024-01-05 19:05:50 +0000

    Update main.py and neetcode.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello CodeSandbox!")
+import subprocess
+
+def run_main():
+    try:
+        # Replace 'python' with 'python3' if you're using Python 3.x
+        subprocess.run(['python', 'neetcode.py'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    run_main()
